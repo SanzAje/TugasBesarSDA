ff4807243638e65e7d99a2191364344f00ab2edd
DaffaFauzyN <daffafauzynugraha@gmail.com>
2025-06-14 21:59:55 +0700

    deklarasi fungsi matematika

---
+++ operations.h
@@ -0,0 +1,15 @@
+#ifndef OPERATIONS_H
+#define OPERATIONS_H
+
+double tambah(double a, double b);
+double kurang(double a, double b);
+double kali(double a, double b);
+double bagi(double a, double b);
+double sinus(double deg);
+double cosinus(double deg);
+double tangen(double deg);
+double log10_custom(double a);
+double akar(double a);
+double pangkat(double a, double b);
+
+#endif
